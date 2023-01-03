ac3f037b379424e585a5206d167fdf2b2da315e9
kelvzzy <kelvinworldstudy@yahoo.com>
2023-01-02 20:58:30 -0800

    Fix

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Integers addition"""
+"""
+Defines an empty class Rectangle
+"""
 
 
-def add_integer(a, b=98):
-    """Adds two numbers
-    Args:
-        a - first number input
-        b - second number input
-    """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+class Rectangle:
+    """Empty representation of a rectangle"""
+    pass
