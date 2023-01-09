e2e32cdedc93286af8f99c5292e7830443b9cffc
kelvzzy <kelvinworldstudy@yahoo.com>
2023-01-09 03:25:08 -0800

    Python - Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module.
+Author: Kelvin
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Improve Geometry.
+Author: Kelvin
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """Raises an exception because...
+        area is not implemented
+        """
+        raise Exception("area() is not implemented")
